54be39fcb528f9b81cbfd4f5f24f7d61a19c1c20
Jack McCauley <104650642+JJMcCauley@users.noreply.github.com>
2022-05-31 12:10:54 -0500

    Fixed responsiveness and layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 baguetteBox.run('.gallery');
+
+// 
+
+
+
+function getUserInput() {
+    const userInput = document.querySelector('#gallery-search').value;
+    return userInput.toLowerCase();
+}
+
+function compareText(userInput) {
+    userInput = getUserInput();
+    const imageList = document.querySelectorAll('a');
+
+    for (i = 0; i < imageList.length; i++) {
+        let imageListText = imageList[i].getAttribute('data-caption').toLowerCase();
+        if (imageListText.includes(userInput) !== true ){
+            imageList[i].style.display = 'none';
+        }
+        else {imageList[i].style.display = 'grid';}
+    }
+}
